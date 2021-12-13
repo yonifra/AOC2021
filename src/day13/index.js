395f5ee87144d870a2c0e37e72997e2c7bcf2f5a
Jonathan Fraimorice <jonathanfr@wix.com>
2021-12-13 09:43:48 +0200

    refactor part 1

--- src/day13/index.js
+++ src/day13/index.js
@@ -3,7 +3,7 @@ const { test, readInput } = require("../utils")
 const prepareInput = (rawInput) => rawInput
 
 const input = prepareInput(readInput())
-const getBoard = (input) => {
+const parsePoints = (input) => {
   const board = {}
   input.forEach((line) => {
     const parts = line.split(" ")
@@ -16,49 +16,27 @@ const getBoard = (input) => {
   return board
 }
 
-const foldX = (board, pos) => {
-  const coords = Object.keys(board).map((key) => key.split("__").map(str => parseInt(str)))
-  coords.forEach((coord) => {
-    if (coord[0] > pos) {
-      const newX = pos - (coord[0] - pos)
-      delete board[`${coord[0]}__${coord[1]}`]
-      board[`${newX}__${coord[1]}`] = "dot"
-    }
-  })
-
-  return board
-}
-
-const foldY = (board, pos) => {
-  const coords = Object.keys(board).map((key) => key.split("__"))
-  coords.forEach((coord) => {
-    if (coord[1] > pos) {
-      const newY = pos - (coord[1] - pos)
-      delete board[`${coord[0]}__${coord[1]}`]
-      board[`${coord[0]}__${newY}`] = "dot"
-    }
-  })
-
-  return board
-}
-
 const fold = (board, coord, pos) => {
-  switch (coord) {
-    case "x": {
-      foldX(board, pos)
-      break
+  const coords = Object.keys(board).map((key) => key.split("__"))
+
+  coords.forEach((point) => {
+    if (point[coord === 'x' ? 0 : 1] > pos) {
+      const newValue = pos - (point[coord === 'x' ? 0 : 1] - pos)
+      delete board[`${point[0]}__${point[1]}`]
+
+      if (coord === 'x') {
+        board[`${newValue}__${point[1]}`] = "dot"
+      } else {
+        board[`${point[0]}__${newValue}`] = "dot"
+      }
     }
-    case "y": {
-      foldY(board, pos)
-      break
-    }
-  }
+  })
 
   return board
 }
 
 const goA = (input) => {
-  const board = getBoard(input)
+  const board = parsePoints(input)
   return Object.keys(fold(board, "x", 655)).length
 }
 
