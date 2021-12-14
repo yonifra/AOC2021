f35ff49b67f8bcc48019d548a102dcf9e7ba3bee
Jonathan Fraimorice <jonathanfr@wix.com>
2021-12-15 00:19:03 +0200

    formatting

--- src/day14/index.js
+++ src/day14/index.js
@@ -3,24 +3,25 @@ const { test, readInput } = require("../utils")
 const prepareInput = (rawInput) => rawInput
 
 const input = prepareInput(readInput())
-const getPolymer = input => input[0]
-const getRules = input => {
+const getPolymer = (input) => input[0]
+const getRules = (input) => {
   const rules = {}
-  input.slice(2).forEach(rule => {
-    const [a, b] = rule.split(' -> ')
+  input.slice(2).forEach((rule) => {
+    const [a, b] = rule.split(" -> ")
     rules[a] = b
   })
   return rules
 }
 
-const commonElements = polymer => {
+const commonElements = (polymer) => {
   const counts = {}
-  polymer.forEach(element => {
+  polymer.forEach((element) => {
     counts[element] = counts[element] ? counts[element] + 1 : 1
   })
 
-  let max = 0, min = Number.MAX_VALUE
-  Object.keys(counts).forEach(key => {
+  let max = 0,
+    min = Number.MAX_VALUE
+  Object.keys(counts).forEach((key) => {
     if (counts[key] > max) {
       max = counts[key]
     }
@@ -29,41 +30,40 @@ const commonElements = polymer => {
       min = counts[key]
     }
   })
-  return {most: max, least: min}
+  return { most: max, least: min }
 }
 
 const goA = (input, steps = 10) => {
   const initialPolymer = getPolymer(input)
   const rules = getRules(input)
-  let polymer = initialPolymer.split('')
+  let polymer = initialPolymer.split("")
   let j = 0
   let newPolymer = []
-  for(let i = 0; i < steps; i++) {
+
+  for (let i = 0; i < steps; i++) {
     newPolymer.push(polymer[0])
-    while(j < polymer.length - 1) {
+    while (j < polymer.length - 1) {
       const a = polymer[j]
       const b = polymer[j + 1]
 
       if (a && b && rules[a + b]) {
-        // newPolymer.push(a)
-        newPolymer.push(rules[a+b])
+        newPolymer.push(rules[a + b])
         newPolymer.push(b)
       } else {
         if (a && b) {
-          // newPolymer.push(a)
           newPolymer.push(b)
         }
       }
 
-      j+=1
+      j += 1
     }
 
-    j=0
+    j = 0
     polymer = newPolymer
     newPolymer = []
   }
 
-  const {most, least} = commonElements(polymer)
+  const { most, least } = commonElements(polymer)
   return most - least
 }
 
