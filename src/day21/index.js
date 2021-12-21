a1b8d8be4e3d65205c33fbeeff08da6b541c8305
Jonathan Fraimorice <jonathanfr@wix.com>
2021-12-21 20:24:13 +0200

    day 21 part 1 done

--- src/day21/index.js
+++ src/day21/index.js
@@ -1,11 +1,43 @@
+const _ = require('lodash')
 const { test, readInput } = require("../utils")
 
 const prepareInput = (rawInput) => rawInput
 
 const input = prepareInput(readInput())
+let diceValue = 0
+let diceRolls = 0
 
-const goA = (input) => {
-  return
+const deterministicRoll = () => {
+  const result = [(diceValue + 1) % 100, (diceValue + 2) % 100, (diceValue + 3) % 100]
+  diceValue += 3
+  diceRolls += 3
+
+  return result
+}
+
+const goA = (input, rollMethod = deterministicRoll) => {
+  let player1Position = 7
+  let player2Position = 9
+  let player1Score = 0
+  let player2Score = 0
+  let currentPlayer = 1
+
+  while (player1Score <= 1000 && player2Score <= 1000) {
+    const rolledValues = rollMethod()
+    if (currentPlayer === 1) {
+      const currentPosition = _.reduce(rolledValues, (a,b) => a+b, player1Position) % 10
+      player1Score += currentPosition
+      player1Position = currentPosition
+      currentPlayer = 2
+    } else {
+      const currentPosition = _.reduce(rolledValues, (a,b) => a+b, player2Position) % 10
+      player2Score += currentPosition
+      player2Position = currentPosition
+      currentPlayer = 1
+    }
+  }
+
+  return diceRolls * _.min([player1Score, player2Score])
 }
 
 const goB = (input) => {
